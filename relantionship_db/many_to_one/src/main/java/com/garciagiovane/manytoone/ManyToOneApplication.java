40ca7f5fc00d7dd8ffac6a1a914152b374589601
Giovane Pacheco <giovane.garcia09@gmail.com>
2019-07-18 15:31:37 -0300

    improve main class name

--- relantionship_db/many_to_one/src/main/java/com/garciagiovane/manytoone/ManyToOneApplication.java
+++ relantionship_db/many_to_one/src/main/java/com/garciagiovane/manytoone/ManyToOneApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RelationshipdbApplication {
+public class ManyToOneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RelationshipdbApplication.class, args);
+		SpringApplication.run(ManyToOneApplication.class, args);
 	}
 
 }
